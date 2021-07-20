38a933149b725dac68c21e907906350564fd026a
yishibakaien <646932161@qq.com>
2021-07-20 17:46:28 +0800

    feat: add example file

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,40 @@
 import path from 'path'
+import { uglify } from 'rollup-plugin-uglify'
+import serve from 'rollup-plugin-serve'
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
 
-export default {
+const isProduction = process.env.NODE_ENV === 'production'
+
+const config = {
   input: path.resolve(__dirname, 'src/index.js'),
   output: {
     format: 'umd',
-    file: path.resolve(__dirname, 'dist/index.js'),
+    sourcemap: true,
+    file: path.resolve(__dirname, `dist/index.${isProduction ? 'min.' : ''}js`),
     name: 'idle',
   },
   plugins: [
+    resolve(),
+    commonjs(),
+    sourceMaps(),
     babel({
       exclude: 'node_modules/**',
     }),
   ],
 }
+
+config.plugins.push(
+  isProduction
+    ? uglify()
+    : serve({
+        open: true,
+        port: 8000,
+        openPage: '/public/index.html',
+        contentBase: '',
+      })
+)
+
+export default config
