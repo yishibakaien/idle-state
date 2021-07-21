2c5a6f13f1d3cc08396cb4a4f21fdf4bbbb335d4
yishibakaien <646932161@qq.com>
2021-07-21 16:38:21 +0800

    feat: basic taskQueue test & demo

--- src/idleState.js
+++ src/idleState.js
@@ -14,31 +14,56 @@ const STATE_START = 1
 const STATE_STOP = 2
 
 class Idle {
-  constructor(callback, timeout) {
+  constructor(tasks, timeout) {
     if (isNaN(timeout)) {
       timeout = 100000
     }
 
+    this.state = STATE_INITIAL
     this.loop = true
     this.await = 100
-    this.taskQueue = []
+    this.taskQueue = tasks || []
     this.taskIndex = 0
     this.timer = null
   }
 
-  start() {}
+  start() {
+    if (this.state === STATE_START) {
+      return this
+    }
+    if (!this.taskQueue || !this.taskQueue.length) {
+      return this
+    }
+    this.state = STATE_START
 
-  stop() {}
+    this._runTask()
+    return this
+  }
 
-  pause() {}
+  stop() {
+    this._dispose()
+  }
 
-  resume() {}
+  pause() {
+    if (this.state === STATE_START) {
+      this.state = STATE_STOP
+    }
+    return this
+  }
+
+  resume() {
+    if (this.state === STATE_STOP) {
+      this.state = STATE_START
+    }
+    return this
+  }
 
   addTask(task) {
     if (typeof task !== 'function') {
       return
     }
     this.taskQueue.push(task)
+    return this
   }
 
   _dispose() {
@@ -58,19 +83,40 @@ class Idle {
     if (
       !this.taskQueue ||
       !this.taskQueue.length ||
-      this.state !== STATE_INITIAL
+      this.state !== STATE_START
     ) {
       return
     }
 
-    if (this.taskIndex === this.taskQueue.length && !this.loop) {
+    if (this.taskIndex === this.taskQueue.length) {
       this._dispose()
       return
     }
+
+    const task = this.taskQueue[this.taskIndex]
+    this._syncTask(task)
   }
 
-  _nextTask() {
+  /**
+   * consider all tasks as sync
+   */
+  _syncTask(task) {
+    task()
+    this._nextTask()
+  }
+
+  _asyncTask(task) {}
+
+  _nextTask(task) {
     this.taskIndex++
+
+    const wait = this.wait || 0
+
+    if (wait) {
+      setTimeout(this._runTask, wait)
+    } else {
+      this._runTask()
+    }
   }
 }
 
